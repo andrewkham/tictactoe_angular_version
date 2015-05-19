692be5d9014fbd2c8df62779f063a34d962306d9
Andrew Ham <hampossible@gmail.com>
2015-05-18 17:52:41 -0700

    90% complete, only exclusive two-player funcationality to add

--- tictactoe_controller.js
+++ tictactoe_controller.js
@@ -9,141 +9,166 @@ function tictactoeController($firebaseObject) {
     self.Lakers = '<img src="images/lakers_logo_mini.png">';
     self.Celtics = '<img src="images/celtics_logo_mini.gif">';
     self.chooseBox = chooseBox;
-    self.turn = 0;
-    self.takeTurns = takeTurns;
     self.getWinner = getWinner;
-    self.Winner = "";
     self.catsGame = catsGame;
+    self.takeTurns = takeTurns;
     self.boardReset = boardReset;
-    self.lakersRingCount = 0;
-    self.celticsRingCount = 0;
+    self.gameReset = gameReset;
     self.updateScoreboard = updateScoreboard;
-    // self.getGame = getGame();
+    self.Game = getGame();
 
-    // function getGame($index) {
-    //     var ref = new $FirebaseObject (url: "")
-    //     function getCells($index){
-    //     }
-    //     function getlakersRings($index){}
-    //     function getcelticsRings($index){}
-    // }
+    function getGame() {
+        var ref = new Firebase("https://nbatictactoe.firebaseio.com/");
+        var Game = $firebaseObject(ref);
+        return Game;
+    }
 
-
-    self.cells = [
-        {isLakers:false, isCeltics:false},
-        {isLakers:false, isCeltics:false},
-        {isLakers:false, isCeltics:false},
-        {isLakers:false, isCeltics:false},
-        {isLakers:false, isCeltics:false},
-        {isLakers:false, isCeltics:false},
-        {isLakers:false, isCeltics:false},
-        {isLakers:false, isCeltics:false},
-        {isLakers:false, isCeltics:false},
-    ];
-    self.lakersRings = [
-        {lakersRing: false}, 
-        {lakersRing: false},
-        {lakersRing: false},  
-        {lakersRing: false},
-        {lakersRing: false},
-        ];
-    self.celticsRings = [
-        {celticsRing: false},
-        {celticsRing: false},
-        {celticsRing: false},
-        {celticsRing: false},
-        {celticsRing: false},
-        ];
-/*
-This function essentially initiates the game by establishing which
-cells are taken and inserting input values per cell.
-*/
     function chooseBox($index) {
-        if ((self.cells[$index].isLakers == true)||(self.cells[$index].isCeltics == true)) {
+        console.log(self.Game)
+        if ((self.Game.cells[$index].isLakers == true) || (self.Game.cells[$index].isCeltics == true)) {
             alert("Box taken!");
         } else {
             takeTurns($index);
         }
     }
 
-    /*
-    This function dictates whose turn it is, Lakers or Celtics. 
-    */
-
     function takeTurns($index) {
-        self.turn++;
-        if ((self.turn + 2) % 2 === 0) {
-            console.log(self.cells[$index])
-            self.cells[$index].isLakers = true;
+        self.Game.turn++;
+        if ((self.Game.turn + 2) % 2 === 0) {
+            self.Game.cells[$index].isLakers = true;
         } else {
-            self.cells[$index].isCeltics = true;
-        } getWinner ($index);
+            self.Game.cells[$index].isCeltics = true;
+        }
+        getWinner();
+        self.Game.$save(self.Game.turn);
+        self.Game.$save(self.Game.cells);
     }
 
-    function getWinner ($index) {
-          // Output Lakers as winners if Lakers player inputs 3 in a row horizontally, vertically or diagonally// 
-    if (((self.cells[0].isLakers == true) && (self.cells[1].isLakers == true) && (self.cells[2].isLakers == true)) ||
-        ((self.cells[3].isLakers == true) && (self.cells[4].isLakers == true) && (self.cells[5].isLakers == true)) ||
-        ((self.cells[6].isLakers == true) && (self.cells[7].isLakers == true) && (self.cells[8].isLakers == true)) ||
-        ((self.cells[0].isLakers == true) && (self.cells[3].isLakers == true) && (self.cells[6].isLakers == true)) ||
-        ((self.cells[1].isLakers == true) && (self.cells[4].isLakers == true) && (self.cells[7].isLakers == true)) ||
-        ((self.cells[2].isLakers == true) && (self.cells[5].isLakers == true) && (self.cells[8].isLakers == true)) ||
-        ((self.cells[0].isLakers == true) && (self.cells[4].isLakers == true) && (self.cells[8].isLakers == true)) ||
-        ((self.cells[6].isLakers == true) && (self.cells[4].isLakers == true) && (self.cells[2].isLakers == true))) {
-        self.lakersRingCount++;
-        console.log(self.lakersRingCount)
-        boardReset($index);
-        updateScoreboard($index);
-    }
-    // Output Celtics as winners if Celtics player inputs 3 in a row horizontally, vertically or diagonally//
-    if (((self.cells[0].isCeltics == true) && (self.cells[1].isCeltics == true) && (self.cells[2].isCeltics == true)) ||
-        ((self.cells[3].isCeltics == true) && (self.cells[4].isCeltics == true) && (self.cells[5].isCeltics == true)) ||
-        ((self.cells[6].isCeltics == true) && (self.cells[7].isCeltics == true) && (self.cells[8].isCeltics == true)) ||
-        ((self.cells[0].isCeltics == true) && (self.cells[3].isCeltics == true) && (self.cells[6].isCeltics == true)) ||
-        ((self.cells[1].isCeltics == true) && (self.cells[4].isCeltics == true) && (self.cells[7].isCeltics == true)) ||
-        ((self.cells[2].isCeltics == true) && (self.cells[5].isCeltics == true) && (self.cells[8].isCeltics == true)) ||
-        ((self.cells[0].isCeltics == true) && (self.cells[4].isCeltics == true) && (self.cells[8].isCeltics == true)) ||
-        ((self.cells[6].isCeltics == true) && (self.cells[4].isCeltics == true) && (self.cells[2].isCeltics == true))) {
-        self.celticsRingCount++;
-        console.log(self.celticsRingCount)
-        boardReset($index);
-        updateScoreboard($index);
+    function getWinner() {
+        // Output Lakers as winners if Lakers player inputs 3 in a row horizontally, vertically or diagonally// 
+        if (((self.Game.cells[0].isLakers == true) && (self.Game.cells[1].isLakers == true) && (self.Game.cells[2].isLakers == true)) ||
+            ((self.Game.cells[3].isLakers == true) && (self.Game.cells[4].isLakers == true) && (self.Game.cells[5].isLakers == true)) ||
+            ((self.Game.cells[6].isLakers == true) && (self.Game.cells[7].isLakers == true) && (self.Game.cells[8].isLakers == true)) ||
+            ((self.Game.cells[0].isLakers == true) && (self.Game.cells[3].isLakers == true) && (self.Game.cells[6].isLakers == true)) ||
+            ((self.Game.cells[1].isLakers == true) && (self.Game.cells[4].isLakers == true) && (self.Game.cells[7].isLakers == true)) ||
+            ((self.Game.cells[2].isLakers == true) && (self.Game.cells[5].isLakers == true) && (self.Game.cells[8].isLakers == true)) ||
+            ((self.Game.cells[0].isLakers == true) && (self.Game.cells[4].isLakers == true) && (self.Game.cells[8].isLakers == true)) ||
+            ((self.Game.cells[6].isLakers == true) && (self.Game.cells[4].isLakers == true) && (self.Game.cells[2].isLakers == true))) {
+            self.Game.lakersRingCount++;
+            self.Game.$save(self.Game.lakersRingCount);
+            boardReset();
+            updateScoreboard();
+        }
+        // Output Celtics as winners if Celtics player inputs 3 in a row horizontally, vertically or diagonally//
+        if (((self.Game.cells[0].isCeltics == true) && (self.Game.cells[1].isCeltics == true) && (self.Game.cells[2].isCeltics == true)) ||
+            ((self.Game.cells[3].isCeltics == true) && (self.Game.cells[4].isCeltics == true) && (self.Game.cells[5].isCeltics == true)) ||
+            ((self.Game.cells[6].isCeltics == true) && (self.Game.cells[7].isCeltics == true) && (self.Game.cells[8].isCeltics == true)) ||
+            ((self.Game.cells[0].isCeltics == true) && (self.Game.cells[3].isCeltics == true) && (self.Game.cells[6].isCeltics == true)) ||
+            ((self.Game.cells[1].isCeltics == true) && (self.Game.cells[4].isCeltics == true) && (self.Game.cells[7].isCeltics == true)) ||
+            ((self.Game.cells[2].isCeltics == true) && (self.Game.cells[5].isCeltics == true) && (self.Game.cells[8].isCeltics == true)) ||
+            ((self.Game.cells[0].isCeltics == true) && (self.Game.cells[4].isCeltics == true) && (self.Game.cells[8].isCeltics == true)) ||
+            ((self.Game.cells[6].isCeltics == true) && (self.Game.cells[4].isCeltics == true) && (self.Game.cells[2].isCeltics == true))) {
+            self.Game.celticsRingCount++;
+            self.Game.$save(self.Game.celticsRingCount);
+            boardReset();
+            updateScoreboard();
 
-    } else {
-        catsGame ($index);
+        } else {
+            catsGame();
+        }
     }
-        // countScore();
-        // boardReset();
-    }
-    function catsGame ($index) {
-        if (self.turn == 9) {
+
+    function catsGame() {
+        if (self.Game.turn == 9) {
             alert("Draw!");
-            boardReset($index);
+            boardReset();
         }
     }
 
-    function updateScoreboard($index) {
-        if (self.lakersRingCount > 0) {
-            self.lakersRings[self.lakersRingCount-1].lakersRing = true;
+    function updateScoreboard() {
+        if (self.Game.lakersRingCount > 0) {
+            self.Game.lakersRings[self.Game.lakersRingCount - 1] = true;
+            self.Game.$save(self.Game.lakersRings);
         }
-        if (self.lakersRingCount == 5) {
+        if (self.Game.lakersRingCount == 5) {
             alert("Lakers are NBA Champions!")
-            location.reload();
+            gameReset();
         }
-        if (self.celticsRingCount > 0) {
-            self.celticsRings[self.celticsRingCount-1].celticsRing = true;
+        if (self.Game.celticsRingCount > 0) {
+            self.Game.celticsRings[self.Game.celticsRingCount - 1] = true;
+            self.Game.$save(self.Game.celticsRings);
         }
-        if (self.celticsRingCount == 5) {
+        if (self.Game.celticsRingCount == 5) {
             alert("Celtics are NBA Champions!")
-            location.reload();
+            gameReset();
         }
     }
-    function boardReset($index) {
-    for ($index = 0; $index < self.cells.length; $index++) {
-        self.cells[$index].isLakers = false;
-        self.cells[$index].isCeltics = false;
-        self.turn=0;
+
+    function boardReset() {
+        for (i = 0; i < self.Game.cells.length; i++) {
+            self.Game.cells[i].isLakers = false;
+            self.Game.cells[i].isCeltics = false;
+            self.Game.turn = 0;
+            self.Game.$save();
         }
+    }
+
+    function gameReset() {
+        console.log("game reset is running");
+        self.Game.celticsRingCount = 0;
+        self.Game.lakersRingCount = 0;
+        if (self.Game.lakersRingCount == 0) {
+            for (i = 0; i < self.Game.lakersRings.length; i++) {
+                self.Game.lakersRings[i] = false;
+                self.Game.$save(self.Game.lakersRings);
+            }
+        }
+        if (self.Game.celticsRingCount == 0) {
+            for (i = 0; i < self.Game.celticsRings.length; i++) {
+                self.Game.celticsRings[i] = false;
+                self.Game.$save(self.Game.celticsRings);
+            }
+        }
+
+
     }
 
 }
+
+
+
+
+// self.cells = [
+//     {isLakers:false, isCeltics:false},
+//     {isLakers:false, isCeltics:false},
+//     {isLakers:false, isCeltics:false},
+//     {isLakers:false, isCeltics:false},
+//     {isLakers:false, isCeltics:false},
+//     {isLakers:false, isCeltics:false},
+//     {isLakers:false, isCeltics:false},
+//     {isLakers:false, isCeltics:false},
+//     {isLakers:false, isCeltics:false},
+// ];
+// self.lakersRings = [
+//     {lakersRing: false}, 
+//     {lakersRing: false},
+//     {lakersRing: false},  
+//     {lakersRing: false},
+//     {lakersRing: false},
+//     ];
+// self.celticsRings = [
+//     {celticsRing: false},
+//     {celticsRing: false},
+//     {celticsRing: false},
+//     {celticsRing: false},
+//     {celticsRing: false},
+//     ];
+/*
+This function essentially initiates the game by establishing which
+cells are taken and inserting input values per cell.
+*/
+
+
+/*
+This function dictates whose turn it is, Lakers or Celtics. 
+*/
